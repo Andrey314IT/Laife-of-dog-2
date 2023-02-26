831c2abc54370b9702b35dc5c967942b97750a08
Andrey314IT <125789209+Andrey314IT@users.noreply.github.com>
2023-02-26 08:46:07 +0000

    Added get older

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ class Dog:
   age = 1
   color = "Black"
   weight = 3
+
+ def __init__(self, type):
+    self.type = type
+   
+def get_older_1(self):
+  self.age += 1
+  self.weight += 2
+  self.height += 10
